ba6c185ed9b3a8f5d0602cecf8001ae83b1de422
Meritoo <github@meritoo.pl>
2019-03-04 19:24:23 +0100

    Tests > missing tests

---
+++ tests/Exception/Bundle/IncorrectBundleNameExceptionTest.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * (c) Meritoo.pl, http://www.meritoo.pl
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Meritoo\Common\Test\Exception\Bundle;
+
+use Meritoo\Common\Exception\Bundle\IncorrectBundleNameException;
+use Meritoo\Common\Test\Base\BaseTestCase;
+use Meritoo\Common\Type\OopVisibilityType;
+
+/**
+ * Test case of an exception used while name of bundle is incorrect
+ *
+ * @author    Meritoo <github@meritoo.pl>
+ * @copyright Meritoo <http://www.meritoo.pl>
+ */
+class IncorrectBundleNameExceptionTest extends BaseTestCase
+{
+    public function testConstructor()
+    {
+        static::assertConstructorVisibilityAndArguments(
+            IncorrectBundleNameException::class,
+            OopVisibilityType::IS_PUBLIC,
+            3
+        );
+    }
+
+    /**
+     * @param string $description     Description of test
+     * @param string $bundleName      Incorrect name of bundle
+     * @param string $expectedMessage Expected exception's message
+     *
+     * @dataProvider provideBundleNameAndMessage
+     */
+    public function testCreate($description, $bundleName, $expectedMessage)
+    {
+        $exception = IncorrectBundleNameException::create($bundleName);
+        static::assertSame($expectedMessage, $exception->getMessage(), $description);
+    }
+
+    public function provideBundleNameAndMessage()
+    {
+        $template = 'Name of bundle \'%s\' is incorrect. It should start with big letter and end with "Bundle". Is'
+            . ' there everything ok?';
+
+        yield[
+            'An empty string as name of bundle',
+            '',
+            sprintf($template, ''),
+        ];
+
+        yield[
+            'Null as name of bundle',
+            null,
+            sprintf($template, ''),
+        ];
+
+        yield[
+            'String with spaces as name of bundle',
+            'This is test',
+            sprintf($template, 'This is test'),
+        ];
+
+        yield[
+            'String without spaces as name of bundle',
+            'ThisIsTest',
+            sprintf($template, 'ThisIsTest'),
+        ];
+    }
+}
---
+++ tests/Exception/Reflection/NotExistingPropertyExceptionTest.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * (c) Meritoo.pl, http://www.meritoo.pl
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Meritoo\Common\Test\Exception\Reflection;
+
+use Meritoo\Common\Exception\Reflection\NotExistingPropertyException;
+use Meritoo\Common\Test\Base\BaseTestCase;
+use Meritoo\Common\Type\OopVisibilityType;
+
+/**
+ * Class NotExistingPropertyExceptionTest
+ *
+ * @author    Meritoo <github@meritoo.pl>
+ * @copyright Meritoo <http://www.meritoo.pl>
+ */
+class NotExistingPropertyExceptionTest extends BaseTestCase
+{
+    public function testConstructor()
+    {
+        static::assertConstructorVisibilityAndArguments(
+            NotExistingPropertyException::class,
+            OopVisibilityType::IS_PUBLIC,
+            3
+        );
+    }
+
+    /**
+     * @param string $description     Description of test
+     * @param mixed  $object          Object that should contains given property
+     * @param string $property        Name of the property
+     * @param string $expectedMessage Expected exception's message
+     *
+     * @dataProvider provideObjectPropertyAndMessage
+     */
+    public function testCreate($description, $object, $property, $expectedMessage)
+    {
+        $exception = NotExistingPropertyException::create($object, $property);
+        static::assertSame($expectedMessage, $exception->getMessage(), $description);
+    }
+
+    public function provideObjectPropertyAndMessage()
+    {
+        $template = 'Property \'%s\' does not exist in instance of class \'%s\'. Did you use proper name of property?';
+
+        yield[
+            'An empty string as name of property',
+            new \stdClass(),
+            '',
+            sprintf($template, '', get_class(new \stdClass())),
+        ];
+
+        yield[
+            'Null as name of property',
+            new \stdClass(),
+            null,
+            sprintf($template, '', get_class(new \stdClass())),
+        ];
+
+        yield[
+            'String with spaces as name of property',
+            new \stdClass(),
+            'This is test',
+            sprintf($template, 'This is test', get_class(new \stdClass())),
+        ];
+
+        yield[
+            'String without spaces as name of property',
+            new \stdClass(),
+            'ThisIsTest',
+            sprintf($template, 'ThisIsTest', get_class(new \stdClass())),
+        ];
+    }
+}
